dd3d96ba844b1292abff7102e45d11ec1173a96b
Takashi Sato <takashi@tks.st>
2024-11-15 18:51:32 +0900

    ci: implement format check of changelog.yaml

--- scripts-for-build/create-changelog.js
+++ scripts-for-build/create-changelog.js
@@ -1,15 +1,42 @@
 import { TZDate } from "@date-fns/tz";
+import Ajv from "ajv";
+import addFormats from "ajv-formats"
 import { format } from "date-fns";
 import yaml from "js-yaml";
 
 import fs from "node:fs";
-import { stdin, stdout } from "node:process";
+import { stdin, stdout, stderr } from "node:process";
+
+import jsonSchema from "./changelog-schema.json" assert { type: "json" };
 
 function toRFC2822DateString(date, zoneString) {
 	return format(new TZDate(date, zoneString), "eee, dd MMM yyyy HH:mm:ss xx");
 }
 
+function printErr(str){
+	stderr.write(`${str}\n`);
+}
+
+// load Ajv and schema
+const ajv = new Ajv({ allErrors: true });
+addFormats(ajv);
+
+const validate = ajv.compile(jsonSchema);
+
+// load YAML
 const x = yaml.load(fs.readFileSync(stdin.fd, "utf-8"));
+
+// validation
+if (!validate(x)) {
+	printErr("input validation failed.");
+	printErr(ajv.errorsText(validate.errors));
+	printErr("");
+	printErr("details:");
+	printErr(JSON.stringify(validate.errors, null, 2));
+	stderr.end();
+	stdout.end();
+	process.exit(1);
+}
 
 const result = [];
 
